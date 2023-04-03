757cd87ee4cce3b7831822bdf7558245f82f1101
Barzu Gabriel <barzu.gabriel@yahoo.com>
2023-04-03 09:57:09 +0300

    Update output.sql

--- output.sql
+++ output.sql
@@ -8,15 +8,15 @@ SET @Area = 'Area'
 SET @ResourceKey = '[lbl_Decription1]'
 EXEC InsertResourceItem @Area, @ResourceKey, 'Description 1'
 EXEC InsertOrUpdateResourceTranslation @LanguageIdForEN, @Area, @ResourceKey, 'English Translation 1'
-EXEC InsertOrUpdateResourceTranslation @LanguageIdForNL, @Area, @ResourceKey, 'Duth translation 1'
+EXEC InsertOrUpdateResourceTranslation @LanguageIdForNL, @Area, @ResourceKey, 'Dutch translation 1'
 
 SET @ResourceKey = '[lbl_Decription1]'
 EXEC InsertResourceItem @Area, @ResourceKey, 'Description 2'
 EXEC InsertOrUpdateResourceTranslation @LanguageIdForEN, @Area, @ResourceKey, 'English Translation 2'
-EXEC InsertOrUpdateResourceTranslation @LanguageIdForNL, @Area, @ResourceKey, 'Duth translation 2'
+EXEC InsertOrUpdateResourceTranslation @LanguageIdForNL, @Area, @ResourceKey, 'Dutch translation 2'
 
 SET @ResourceKey = '[lbl_Decription1]'
 EXEC InsertResourceItem @Area, @ResourceKey, 'Description 3'
 EXEC InsertOrUpdateResourceTranslation @LanguageIdForEN, @Area, @ResourceKey, 'English Translation 3'
-EXEC InsertOrUpdateResourceTranslation @LanguageIdForNL, @Area, @ResourceKey, 'Duth translation 3'
+EXEC InsertOrUpdateResourceTranslation @LanguageIdForNL, @Area, @ResourceKey, 'Dutch translation 3'
 
